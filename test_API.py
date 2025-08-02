4506c49de215eec0a095f49836b29926e7cb76ce
siddu0228 <ksiddu0228@gmail.com>
2025-08-02 18:43:53 +0530

    Updated api_client and test_API files

--- test_API.py
+++ test_API.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from utils import api_client
+import api_client
 
 def test_get_users():
     response = api_client.get_users()
@@ -10,14 +10,5 @@ def test_get_user():
     response = api_client.get_user(2)
     assert response.status_code == 200
     assert response.json()['data']['id'] == 2
-def test_create_user():
-    payload = {"name": "sidda","job":"QA engineer"}
-    response = api_client.create_user(payload)
-    assert response.status_code == 201
-    assert response.json()["data"]["name"] == "sidda"
-def test_update_user():
-    payload = {"name": "sidda","job":"SR QA engineer"}
-    response = api_client.update_user(payload)
-    assert response.status_code == 201
-    assert response.json()["data"]["job"] == "SR QA engineer"
+
 
