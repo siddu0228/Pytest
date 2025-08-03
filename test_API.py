61188a75ea4e251319150424d748b1d1874aed28
siddu0228 <ksiddu0228@gmail.com>
2025-08-03 18:11:46 +0530

    case3 added

--- test_API.py
+++ test_API.py
@@ -10,7 +10,7 @@ def test_get_user():
     response = api_client.get_user(2)
     assert response.status_code == 200
     assert response.json()['data']['id'] == 2
-def test_get_users():
+def test_add_math():
     assert api_client.add(2) == 4
     
 
